3d45b7103bd2608e8a46072c1578c86649e8eeb8
品牌营销业务-JAVA-陈耀武 <jack.chen@yolipai.com>
2017-01-15 22:16:42 +0800

    search

--- src/home/controller/movie.js
+++ src/home/controller/movie.js
@@ -8,17 +8,17 @@ import Base from './base.js';
 export default class extends Base {
 
 
-    
+
     /**
      *  电影列表 分页
      * @param page 当前页
      * @param page_size 每页显示大小
      * @returns {接口数据}
      */
-    async pageAction(){
+    async pageAction(page=1,size=5){
 
-        let page = this.get("page") || 0;
-        let size =this.get("size") || 5;
+        page = this.get("page");
+        size =this.get("size");
 
         think.log(page)
         think.log(size)
@@ -26,6 +26,21 @@ export default class extends Base {
         return this.success(list)
 
     }
+
+
+    /**
+     *  电影名称搜索列表，返回10条数据,如果想更精确，就输入更多内容
+     * @param size
+     */
+    async searchAction(){
+
+        let size =this.get("size") || 10;
+        let title = this.get("title");
+        think.log(size)
+        let resutl = await this.model("movie").where({title: ["like", "%"+title+"%"]}).order("show_time DESC").limit(size).select();
+        return this.success(resutl);
+    }
+
 
 
 }

7fd3fb8a0867105ba819a4d7daba3a1404da8af5
品牌营销业务-JAVA-陈耀武 <jack.chen@yolipai.com>
2017-02-02 22:45:44 +0800

    comment interface

---
+++ src/home/controller/comment.js
@@ -0,0 +1,76 @@
+'use strict';
+
+import Base from './base.js';
+import moment from 'moment';
+
+
+
+/**
+ * 评论模块
+ */
+export default class extends Base {
+
+    async init(...args) {
+        super.init(...args);
+
+    }
+
+    async __before(){
+        let token =  this.post("token") || this.get("token");
+        let user = await this.model("user").where({token:token}).find();
+        if( think.isEmpty(user))return;
+
+        this.openid = user.openid;
+        this.nickname = new Buffer( user.nickname,'UTF-8').toString();
+        this.head_img = user.head_img;
+    }
+
+    /**
+     * 新增评论
+     * @param movie_id
+     * @param content
+     * @param score
+     * @param order_id
+     * @param
+     */
+    async addAction(){
+
+        let data = this.post();
+        think.log(data)
+        //查询订单是否评论过
+        let result = await this.model("comment").where({order_id:data.order_id}).find();
+        //为空，则新增评论
+        if( think.isEmpty(result) ) {
+            let comment = {};
+            comment.openid = this.openid;
+            comment.nickname = this.nickname;
+            comment.head_img = this.head_img;
+
+            comment.movie_id = data.movie_id;
+            comment.create_time = moment().format('YYYY-MM-DD HH:mm:ss');
+            comment.content = data.content;
+            comment.score = data.score;
+            comment.order_id = data.order_id;
+            let insert_id = await this.model("comment").add(comment);
+            comment.id = insert_id;
+            return this.success(comment);
+        }
+        return this.fail();
+    }
+
+
+
+
+    /**
+     * 根据电影id查询评论列表
+     * 不做分页，查询前15条评论
+     */
+    async querylistAction(){
+
+        let movie_id = this.get("movie_id");
+        let list = await this.model("comment").where({movie_id:movie_id}).order("create_time","DESC").limit(15).select();
+        return this.success(list);
+    }
+
+
+}
